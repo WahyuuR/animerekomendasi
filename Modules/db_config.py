e0310a2895c997fb4f3a8a8eb368a0275d93759f
WahyuuR <wahyu12rahman@gmail.com>
2025-07-03 14:28:56 +0700

    upload

--- Modules/db_config.py
+++ Modules/db_config.py
@@ -1,6 +1,7 @@
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'anime_db'
+    'host': 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+    'port': 4000,
+    'user': 'Rzvsxq5UxjRJFEm.root',
+    'password': 'mToa4zNK9VhT5L02',
+    'database': 'test'
 }   
